e04a0a43661820999c85f4c615de34270878c968
hyer0705 <hyer1205@gmail.com>
2023-10-26 18:44:16 +0900

    Add CSS reset using styled-components and styled-reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import { router } from "./router.tsx";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <GlobalStyle />
   </React.StrictMode>
 );
