0a5b55ce9235a102ef551ddaf6b083b3668aac74
hyer0705 <hyer1205@gmail.com>
2023-10-27 23:08:12 +0900

    Use useRecoilState to implement toggle theme button

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import reset from "styled-reset";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
 import { router } from "./router.tsx";
-import { darkTheme, lightTheme } from "./app-theme.ts";
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  * {
-    box-sizing: border-box;
-    color: ${(props) => props.theme.colors.text};
-  }
-  body {
-    background-color: ${(props) => props.theme.colors.bg};
-  }
-  a {
-    text-decoration: none;
-    background-color: inherit;
-  }
-`;
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={lightTheme}>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        <GlobalStyle />
-      </ThemeProvider>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
